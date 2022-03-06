c673dc058d38c9379638f60d3fb78ce58552ab6c
Matt Wilson <mattwwilson34@gmail.com>
2022-03-06 11:24:25 -0500

    Added Nav styles

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 This is a sample website for a personal trainer.
+
+Hero Image:
+Photo by <a href="https://unsplash.com/@sxoxm?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Sven Mieke</a> on <a href="https://unsplash.com/s/photos/exercise?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,14 +1,19 @@
+import '../../index.css';
+import './Nav.css';
 import { Link } from 'react-router-dom';
 import logo from '../../Icons/logo.svg';
 const Nav = () => {
   return (
     <nav className='Nav'>
-      <div className='Logo'>
-        <img className='Nav_Logo_SVG' src={logo} alt='logo'></img>
-        <span className='Nav_Logo_Text'>Symmetric Athletics</span>
-      </div>
-      <Link to='about'>About</Link> {' | '}
+      <Link className='Nav_Logo_Container' to='/'>
+        <span className='Nav_Logo_Text'>
+          <img className='Nav_Logo_SVG' src={logo} alt='logo'></img>Symmetric
+          Athletics
+        </span>
+      </Link>
+      <Link to='about'>About</Link>
       <Link to='contact'>Contact</Link>
+      <Link to='scedule'>Schedule</Link>
     </nav>
   );
 };
