60ca78bb9169cab6c0cec2b77aaf48f344c7bc40
Matt Wilson <mattwwilson34@gmail.com>
2022-03-06 10:39:17 -0500

    Added svg logo

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,8 +1,12 @@
 import { Link } from 'react-router-dom';
+import logo from '../../Icons/logo.svg';
 const Nav = () => {
   return (
-    <nav>
-      <div>Symmetric Athletics</div>
+    <nav className='Nav'>
+      <div className='Logo'>
+        <img className='Nav_Logo_SVG' src={logo} alt='logo'></img>
+        <span className='Nav_Logo_Text'>Symmetric Athletics</span>
+      </div>
       <Link to='about'>About</Link> {' | '}
       <Link to='contact'>Contact</Link>
     </nav>
