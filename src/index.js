8a9fe9fd988f0938de5225084f9e2795056f892b
Matt Wilson <mattwwilson34@gmail.com>
2022-03-07 07:47:00 -0500

    Removed fixed position of navbar to allow it to scale to mobile

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import Contact from './Routes/Contact';
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
-      <Route path='/' element={<App />}></Route>
+      <Route path='/symmetric-athletics' element={<App />}></Route>
       <Route path='about' element={<About />} />
       <Route path='contact' element={<Contact />} />
     </Routes>
