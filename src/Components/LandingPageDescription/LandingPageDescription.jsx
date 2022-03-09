c40760afa5146e8f726be77d896a744824a5571d
Matt Wilson <mattwwilson34@gmail.com>
2022-03-08 20:28:55 -0500

    Extracted LandingPageDescription into Component

---
+++ src/Components/LandingPageDescription/LandingPageDescription.jsx
@@ -0,0 +1,28 @@
+function LandingPageDescription() {
+  return (
+    <section className='Description'>
+      <h3>IT'S TIME TO REACH YOUR FULL POTENTIAL</h3>
+      <p>
+        Lacus lacinia bibendum ex porttitor laoreet fermentum torquent, primis
+        vestibulum neque in varius praesent, duis justo dictumst interdum sit
+        pharetra.
+      </p>
+      <p>
+        Luctus massa ligula quisque aliquam sapien curae maecenas at, rutrum
+        scelerisque senectus gravida nunc dapibus metus, varius ipsum mollis sem
+        elit eu volutpat. Risus ultrices lacinia maecenas nibh facilisi id,
+        habitasse arcu at tortor sagittis nullam, feugiat eu laoreet in
+        penatibus.
+      </p>
+      <p>
+        Ultrices massa malesuada lectus ex hac pretium quis sagittis, fames at
+        donec sem tempor posuere porttitor proin molestie, ac ad ipsum vehicula
+        ante felis dapibus. Eget libero lorem vitae ac turpis aptent sed ex
+        facilisis vestibulum nisl, dolor phasellus finibus mauris auctor non nec
+        proin tincidunt.
+      </p>
+      <button>ABOUT US</button>
+    </section>
+  );
+}
+export default LandingPageDescription;
