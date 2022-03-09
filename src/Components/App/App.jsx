371e509adb033cb783c1334336b509bdae164e2b
Matt Wilson <mattwwilson34@gmail.com>
2022-03-08 20:39:42 -0500

    Extracted LandingPageResults into Component

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -4,8 +4,9 @@ import Nav from '../Nav/Nav';
 import Hero from '../Hero/Hero';
 import LandingPageDescription from '../LandingPageDescription/LandingPageDescription';
 import WhatWeDo from '../WhatWeDo/WhatWeDo';
+import LandingPageResults from '../LandingPageResults/LandingPageResults';
 import Footer from '../Footer/Footer';
-import DeanSled from '../../Images/Dean-Sled.jpeg';
+
 import Friends from '../../Images/Friends.jpeg';
 
 function App() {
@@ -15,23 +16,7 @@ function App() {
       <Hero></Hero>
       <LandingPageDescription></LandingPageDescription>
       <WhatWeDo></WhatWeDo>
-      <section className='Results'>
-        <div className='Results_Container'>
-          <h2>GETTING RESULTS SINCE 2021</h2>
-          <p>
-            Dictumst dignissim accumsan elit nisi aenean interdum ac nulla mus
-            elementum amet tempus, vivamus finibus parturient nunc dis augue
-            sollicitudin aliquet lectus natoque. Facilisi morbi maximus dictum
-            augue varius mauris accumsan netus eu conubia, phasellus sociosqu
-            curae nec platea erat ipsum vitae eleifend. Mauris inceptos tortor
-            velit gravida ligula aliquet malesuada turpis facilisis justo risus
-            blandit nulla tempus maecenas conubia himenaeos, quisque platea
-            imperdiet pellentesque litora scelerisque penatibus sagittis iaculis
-            dictum a ante sollicitudin ultrices quam.
-          </p>
-          <img src={DeanSled} alt='Dean Sled'></img>
-        </div>
-      </section>
+      <LandingPageResults></LandingPageResults>
       <section className='Mission'>
         <div className='Mission_Container'>
           <h2>OUR MISSION</h2>
---
+++ src/Components/LandingPageResults/LandingPageResults.jsx
@@ -0,0 +1,25 @@
+import DeanSled from '../../Images/Dean-Sled.jpeg';
+
+function LandingPageResults() {
+  return (
+    <section className='Results'>
+      <div className='Results_Container'>
+        <h2>GETTING RESULTS SINCE 2021</h2>
+        <p>
+          Dictumst dignissim accumsan elit nisi aenean interdum ac nulla mus
+          elementum amet tempus, vivamus finibus parturient nunc dis augue
+          sollicitudin aliquet lectus natoque. Facilisi morbi maximus dictum
+          augue varius mauris accumsan netus eu conubia, phasellus sociosqu
+          curae nec platea erat ipsum vitae eleifend. Mauris inceptos tortor
+          velit gravida ligula aliquet malesuada turpis facilisis justo risus
+          blandit nulla tempus maecenas conubia himenaeos, quisque platea
+          imperdiet pellentesque litora scelerisque penatibus sagittis iaculis
+          dictum a ante sollicitudin ultrices quam.
+        </p>
+        <img src={DeanSled} alt='Dean Sled'></img>
+      </div>
+    </section>
+  );
+}
+
+export default LandingPageResults;
